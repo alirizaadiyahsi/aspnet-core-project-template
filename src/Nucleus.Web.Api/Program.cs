3afb010401f1f01a5d5c7bf862629610b2d4b622
alirizaadiyahsi <aliriza.adiyahsi@volosoft.com>
2019-08-02 20:21:22 +0300

    Added project

---
+++ src/Nucleus.Web.Api/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nucleus.Application.Permissions;
+using Nucleus.Core.Permissions;
+using Serilog;
+using Serilog.Events;
+
+namespace Nucleus.Web.Api
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    "Logs/Logs.txt",
+                    fileSizeLimitBytes: 1_000_000,
+                    rollOnFileSizeLimit: true,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+
+                Log.Information("Starting web host");
+                var host = CreateHostBuilder(args).Build();
+                using (var scope = host.Services.CreateScope())
+                {
+                    var permissionAppService = scope.ServiceProvider.GetRequiredService<IPermissionAppService>();
+                    permissionAppService.InitializePermissions(DefaultPermissions.All());
+                }
+
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
+                });
+    }
+}
